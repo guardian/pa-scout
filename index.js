93c4082c7c875a0cf788bb5247d3dd38501b1cc7
Max Spencer <max.spencer@guardian.co.uk>
2017-09-01 12:02:15 +0100

    Inital commit

---
+++ index.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var request = require('request');
+var parseXml = require('xml2js').parseString;
+var util = require('util');
+
+const competitions_url = 'https://mobile.guardianapis.com/sport/football/competitions';
+const competition_teams_url_base = 'http://football.api.press.net/v1.5/competition/teams';
+
+function get_competitions(filter, callback) {
+    request(competitions_url, function (error, response, body) {
+	if (error) {
+	    callback(error);
+	}
+	
+	var result = JSON.parse(body);
+	if (filter) {
+	    result = result.filter(function(competition) {
+		return competition.fullName.toLowerCase().includes(filter.toLowerCase());
+	    });
+	}
+
+	callback(null, result)
+    });
+}
+
+function to_pa_date(mapi_date) {
+    return mapi_date.replace(/-/g, '');
+}
+
+function get_competition_teams_url(competition) {
+    url = competition_teams_url_base +
+	'/' + process.env.PA_KEY +
+	'/' + competition.id +
+	'/' + to_pa_date(competition.startDate) +
+	'/' + to_pa_date(competition.endDate);
+    return url
+}
+
+function get_competition_teams(competition, filter, callback) {
+    request(get_competition_teams_url(competition), function (error, response, body) {
+	if (error) {
+	    callback(error);
+	}
+
+	parseXml(body, function (error, xml) {
+	    if (error) {
+		callback(error);
+	    }
+	    var results = [];
+	    xml.teams.team.filter(function(tag) {
+		return tag._.toLowerCase().includes(filter.toLowerCase());
+	    }).forEach(function(tag) {
+		results.push({
+		    name: tag._,
+		    id: tag.$.teamID
+		});
+	    });
+	    callback(null, results);
+	});
+    });
+}
+
+program
+    .arguments('<query>')
+    .option('-c, --competition <competition>', 'The competition to restrict results to')
+    .action(function(query) {
+	get_competitions(program.competition, function(err, result) {
+	    result.forEach(function(competition) {
+		get_competition_teams(competition, query, function(err, result) {
+		    if (result.length > 0) {
+			console.log(competition.fullName + ":");
+			console.log(result);
+		    }
+		});
+	    });
+	});
+    })
+    .parse(process.argv)
